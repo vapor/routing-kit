04f1cdcaa7b5fc1ca085787aad243ad7aad1c550
Logan Wright <logan.william.wright@gmail.com>
2017-03-06 12:18:26 +0100

    unnest generic class

--- Sources/Routing/SubBranchMap.swift
+++ Sources/Routing/SubBranchMap.swift
@@ -1,47 +1,45 @@
-extension Branch {
-    struct SubBranchMap {
-        var paramBranches: [String: Branch] = [:]
-        var slugBranches: [Branch] = []
-        var wildcard: Branch?
+struct SubBranchMap<Output> {
+    var paramBranches: [String: Branch<Output>] = [:]
+    var slugBranches: [Branch<Output>] = []
+    var wildcard: Branch<Output>?
 
-        var allBranches: [Branch] {
-            var all = [Branch]()
-            all += paramBranches.values
-            all += slugBranches
-            if let wildcard = wildcard {
-                all.append(wildcard)
-            }
-            return all
+    var allBranches: [Branch<Output>] {
+        var all = [Branch<Output>]()
+        all += paramBranches.values
+        all += slugBranches
+        if let wildcard = wildcard {
+            all.append(wildcard)
         }
+        return all
+    }
 
-        init() {}
+    init() {}
 
-        subscript(key: String) -> Branch? {
-            get {
-                if key == "*" {
-                    return wildcard
-                } else if key.hasPrefix(":") {
-                    return slugBranches.lazy.filter { $0.name == key } .first
-                } else {
-                    return paramBranches[key]
-                }
+    subscript(key: String) -> Branch<Output>? {
+        get {
+            if key == "*" {
+                return wildcard
+            } else if key.hasPrefix(":") {
+                return slugBranches.lazy.filter { $0.name == key } .first
+            } else {
+                return paramBranches[key]
             }
-            set {
-                if key == "*" {
-                    wildcard = newValue
-                } else if key.hasPrefix(":") {
-                    if let existing = slugBranches.index(where: { $0.name == key }) {
-                        if let value = newValue {
-                            slugBranches[existing] = value
-                        } else {
-                            slugBranches.remove(at: existing)
-                        }
-                    } else if let value = newValue {
-                        slugBranches.append(value)
+        }
+        set {
+            if key == "*" {
+                wildcard = newValue
+            } else if key.hasPrefix(":") {
+                if let existing = slugBranches.index(where: { $0.name == key }) {
+                    if let value = newValue {
+                        slugBranches[existing] = value
+                    } else {
+                        slugBranches.remove(at: existing)
                     }
-                } else {
-                    paramBranches[key] = newValue
+                } else if let value = newValue {
+                    slugBranches.append(value)
                 }
+            } else {
+                paramBranches[key] = newValue
             }
         }
     }
