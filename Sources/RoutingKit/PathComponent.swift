e8069d35dd7d699598b028c63522fd1388eb571c
Dimitri Bouniol <dimitribouniol@mochidev.com>
2020-07-15 15:12:31 -0700

    Updated PathComponent's ExpressibleByStringLiteral to ExpressibleByStringInterpolation so Path Components can be expressed with interpolation

--- Sources/RoutingKit/PathComponent.swift
+++ Sources/RoutingKit/PathComponent.swift
@@ -1,6 +1,6 @@
 /// A single path component of a `Route`. An array of these components describes
 /// a route's path, including which parts are constant and which parts are dynamic.
-public enum PathComponent: ExpressibleByStringLiteral, CustomStringConvertible {
+public enum PathComponent: ExpressibleByStringInterpolation, CustomStringConvertible {
     /// A normal, constant path component.
     case constant(String)
 
