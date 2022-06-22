f1248d2ef62fd3122282b9cc7bdbf194aed091d5
Maxim Kolesnik <kolesnik@yandex-team.ru>
2022-06-22 16:00:48 +0300

    add iOS 11 platform

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "routing-kit",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_15),
+       .iOS(.v11)
     ],
     products: [
         .library(name: "RoutingKit", targets: ["RoutingKit"]),
