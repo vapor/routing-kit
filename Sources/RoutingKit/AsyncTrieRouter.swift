43b09df648a47969dbdaf80d59e51f1676d95811
Tim <0xtimc@gmail.com>
2023-11-06 15:09:30 +0000

    Add new AsyncRouter that can be Sendable

--- Sources/RoutingKit/AsyncTrieRouter.swift
+++ Sources/RoutingKit/AsyncTrieRouter.swift
@@ -1,12 +1,12 @@
 import Logging
 
-/// Generic `TrieRouter` built using the "trie" tree algorithm.
+/// Generic `TrieRouter` built using the "trie" tree algorithm that is `Sendable`
 ///
 /// Use `register(...)` to register routes into the router. Use `route(...)` to then fetch a matching
 /// route's output.
 ///
 /// See https://en.wikipedia.org/wiki/Trie for more information.
-public final class TrieRouter<Output>: Router, CustomStringConvertible {
+public final class AsyncTrieRouter<Output: Sendable>: AsyncRouter, Sendable {
     /// Available `TrieRouter` customization options.
     public enum ConfigurationOption: Sendable {
         /// If set, this will cause the router's route matching to be case-insensitive.
@@ -15,7 +15,7 @@ public final class TrieRouter<Output>: Router, CustomStringConvertible {
     }
     
     /// Configured options such as case-sensitivity.
-    public var options: Set<ConfigurationOption>
+    public let options: Set<ConfigurationOption>
 
     /// The root node.
     private let root: Node
@@ -48,7 +48,7 @@ public final class TrieRouter<Output>: Router, CustomStringConvertible {
     ///
     /// - parameters:
     ///     - route: `Route` to register to this router.
-    public func register(_ output: Output, at path: [PathComponent]) {
+    public func register(_ output: Output, at path: [PathComponent]) async {
         assert(!path.isEmpty, "Cannot register a route with an empty path.")
         
         // start at the root of the trie branch
@@ -62,18 +62,18 @@ public final class TrieRouter<Output>: Router, CustomStringConvertible {
                 precondition(index == path.count - 1, "Catchall ('\(component)') must be the last component in a path.")
                 fallthrough
             default:
-                current = current.buildOrFetchChild(for: component, options: self.options)
+                current = await current.buildOrFetchChild(for: component, options: self.options)
             }
         }
 
         // if this node already has output, we are overriding a route
-        if current.output != nil {
+        if await current.output != nil {
             self.logger.info("[Routing] Overriding duplicate route for \(path[0]) \(path.dropFirst().string)")
         }
         
         // after iterating over all path components, we can set the output
         // on the current node
-        current.output = output
+        await current.setOutput(output)
     }
 
     /// Routes a `path`, returning the best-matching output and collecting any dynamic parameters.
@@ -85,7 +85,7 @@ public final class TrieRouter<Output>: Router, CustomStringConvertible {
     ///     - path: Array of `RoutableComponent` to route against.
     ///     - params: A mutable `Parameters` to collect dynamic parameters.
     /// - returns: Best-matching output for the supplied path.
-    public func route(path: [String], parameters: inout Parameters) -> Output? {
+    public func route(path: [String], parameters: inout Parameters) async -> Output? {
         // always start at the root node
         var currentNode: Node = self.root
         
@@ -96,20 +96,20 @@ public final class TrieRouter<Output>: Router, CustomStringConvertible {
         // traverse the string path supplied
         search: for (index, slice) in path.enumerated() {
             // store catchall in case search hits dead end
-            if let catchall = currentNode.catchall {
+            if let catchall = await currentNode.catchall {
                 currentCatchall = (catchall, [String](path.dropFirst(index)))
             }
 
             // check the constants first
-            if let constant = currentNode.constants[isCaseInsensitive ? slice.lowercased() : slice] {
+            if let constant = await currentNode.constants[isCaseInsensitive ? slice.lowercased() : slice] {
                 currentNode = constant
                 continue search
             }
 
             // no constants matched, check for dynamic members
             // including parameters or anythings
-            if let wildcard = currentNode.wildcard {
-                if let name = wildcard.parameter {
+            if let wildcard = await currentNode.wildcard {
+                if let name = await wildcard.parameter {
                     parameters.set(name, to: slice)
                 }
 
@@ -121,19 +121,19 @@ public final class TrieRouter<Output>: Router, CustomStringConvertible {
             if let (catchall, subpaths) = currentCatchall {
                 // fallback to catchall output if we have one
                 parameters.setCatchall(matched: subpaths)
-                return catchall.output
+                return await catchall.output
             } else {
                 return nil
             }
         }
 
-        if let output = currentNode.output {
+        if let output = await currentNode.output {
             // return the currently resolved responder if there hasn't been an early exit.
             return output
         } else if let (catchall, subpaths) = currentCatchall {
             // fallback to catchall output if we have one
             parameters.setCatchall(matched: subpaths)
-            return catchall.output
+            return await catchall.output
         } else {
             // current node has no output and there was not catchall
             return nil
@@ -141,16 +141,18 @@ public final class TrieRouter<Output>: Router, CustomStringConvertible {
     }
     
     public var description: String {
-        return self.root.description
+        get async {
+            await self.root.description
+        }
     }
 }
 
-extension TrieRouter {
+extension AsyncTrieRouter {
     /// A single node of the `Router`s trie tree of routes.
-    final class Node: CustomStringConvertible {
+    actor Node {
 
         /// Describes a node that has matched a parameter or anything
-        final class Wildcard {
+        actor Wildcard {
             private(set) var parameter: String?
             private(set) var explicitlyIncludesAnything = false
 
@@ -160,15 +162,15 @@ extension TrieRouter {
                 self.node = node
             }
 
-            static func anything(_ node: Node) -> Wildcard {
+            static func anything(_ node: Node) async -> Wildcard {
                 let wildcard = Wildcard(node: node)
-                wildcard.explicitlyIncludesAnything = true
+                await wildcard.explicitlyIncludeAnything()
                 return wildcard
             }
 
-            static func parameter(_ node: Node, named name: String) -> Wildcard {
+            static func parameter(_ node: Node, named name: String) async -> Wildcard {
                 let wildcard = Wildcard(node: node)
-                wildcard.setParameterName(name)
+                await wildcard.setParameterName(name)
                 return wildcard
             }
 
@@ -202,9 +204,14 @@ extension TrieRouter {
             self.constants = [String: Node]()
         }
         
+        // Allow us to mutate this from outside the actor
+        func setOutput(_ output: Output) {
+            self.output = output
+        }
+        
         /// Fetches the child `RouterNode` for the supplied path component, or builds
         /// a new segment onto the tree if necessary.
-        func buildOrFetchChild(for component: PathComponent, options: Set<ConfigurationOption>) -> Node {
+        func buildOrFetchChild(for component: PathComponent, options: Set<ConfigurationOption>) async -> Node {
             let isCaseInsensitive = options.contains(.caseInsensitive)
             
             switch component {
@@ -226,15 +233,15 @@ extension TrieRouter {
                 let node: Node
 
                 if let wildcard = self.wildcard {
-                    if let existingName = self.wildcard?.parameter {
+                    if let existingName = await self.wildcard?.parameter {
                         precondition(existingName == name, "It is not possible to have two routes with the same prefix but different parameter names, even if the trailing path components differ (tried to add route with \(name) that collides with \(existingName)).")
                     } else {
-                        wildcard.setParameterName(name)
+                        await wildcard.setParameterName(name)
                     }
                     node = wildcard.node
                 } else {
                     node = Node()
-                    self.wildcard = .parameter(node, named: name)
+                    self.wildcard = await .parameter(node, named: name)
                 }
                 return node
             case .catchall:
@@ -249,36 +256,38 @@ extension TrieRouter {
             case .anything:
                 let node: Node
                 if let wildcard = self.wildcard {
-                    wildcard.explicitlyIncludeAnything()
+                    await wildcard.explicitlyIncludeAnything()
                     node = wildcard.node
                 } else {
                     node = Node()
-                    self.wildcard = .anything(node)
+                    self.wildcard = await .anything(node)
                 }
                 return node
             }
         }
         
         var description: String {
-            self.subpathDescriptions.joined(separator: "\n")
+            get async {
+                await self.subpathDescriptions().joined(separator: "\n")
+            }
         }
         
-        var subpathDescriptions: [String] {
+        func subpathDescriptions() async -> [String] {
             var desc: [String] = []
             for (name, constant) in self.constants {
                 desc.append("→ \(name)")
-                desc += constant.subpathDescriptions.indented()
+                desc += await constant.subpathDescriptions().indented()
             }
 
             if let wildcard = self.wildcard {
-                if let name = wildcard.parameter {
+                if let name = await wildcard.parameter {
                     desc.append("→ :\(name)")
-                    desc += wildcard.node.subpathDescriptions.indented()
+                    desc += await wildcard.node.subpathDescriptions().indented()
                 }
 
-                if wildcard.explicitlyIncludesAnything {
+                if await wildcard.explicitlyIncludesAnything {
                     desc.append("→ *")
-                    desc += wildcard.node.subpathDescriptions.indented()
+                    desc += await wildcard.node.subpathDescriptions().indented()
                 }
             }
 
