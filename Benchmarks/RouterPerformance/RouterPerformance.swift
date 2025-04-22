a255130cd8a699f9e2e67dc1771685fe0c62bc68
Gwynne Raskind <gwynne@vapor.codes>
2025-04-22 14:37:47 -0500

    Convert the performance tests to run with package-benchmarks

---
+++ Benchmarks/RouterPerformance/RouterPerformance.swift
@@ -0,0 +1,106 @@
+import Benchmark
+import RoutingKit
+
+let benchmarks = {
+    Benchmark("Case-sensitive") { benchmark in
+        let router = TrieRouter(String.self)
+        for letter in ["a", "b", "c", "d", "e" , "f", "g"] {
+            router.register(letter, at:[
+                .constant(letter),
+                .parameter("\(letter)_id")
+            ])
+        }
+
+        for _ in benchmark.scaledIterations {
+            var params = Parameters()
+            _ = router.route(path: ["a", "42"], parameters: &params)
+        }
+    }
+
+    Benchmark("Case-insensitive") { benchmark in
+        let router = TrieRouter.init(String.self, options: [.caseInsensitive])
+        for letter in ["a", "b", "c", "d", "e" , "f", "g"] {
+            router.register(letter, at: [
+                .constant(letter),
+                .parameter("\(letter)_id")
+            ])
+        }
+        
+        for _ in benchmark.scaledIterations {
+            var params = Parameters()
+            _ = router.route(path: ["a", "42"], parameters: &params)
+        }
+    }
+
+    Benchmark("Case-insensitive Match First") { benchmark in
+        let router = TrieRouter.init(String.self, options: [.caseInsensitive])
+        for letter in ["aaaaaaaa", "aaaaaaab", "aaaaaaac", "aaaaaaad", "aaaaaaae" , "aaaaaaaf", "aaaaaaag"] {
+            router.register(letter, at: [
+                .constant(letter),
+                .parameter("\(letter)_id")
+            ])
+        }
+        
+        for _ in benchmark.scaledIterations {
+            var params = Parameters()
+            _ = router.route(path: ["aaaaaaaa", "42"], parameters: &params)
+        }
+    }
+
+    Benchmark("Case-insensitive Match Last") { benchmark in
+        let router = TrieRouter.init(String.self, options: [.caseInsensitive])
+        for letter in ["aaaaaaaa", "aaaaaaab", "aaaaaaac", "aaaaaaad", "aaaaaaae" , "aaaaaaaf", "aaaaaaag"] {
+            router.register(letter, at: [
+                .constant(letter),
+                .parameter("\(letter)_id")
+            ])
+        }
+        
+        for _ in benchmark.scaledIterations {
+            var params = Parameters()
+            _ = router.route(path: ["aaaaaaag", "42"], parameters: &params)
+        }
+    }
+
+    Benchmark("Case-sensitive Minimal") { benchmark in
+        let router = TrieRouter.init(String.self)
+        for letter in ["a"] {
+            router.register(letter, at: [
+                .constant(letter)
+            ])
+        }
+        
+        for _ in benchmark.scaledIterations {
+            var params = Parameters()
+            _ = router.route(path: ["a"], parameters: &params)
+        }
+    }
+
+    Benchmark("Case-insensitive Minimal") { benchmark in
+        let router = TrieRouter.init(String.self, options: [.caseInsensitive])
+        for letter in ["a"] {
+            router.register(letter, at: [
+                .constant(letter)
+            ])
+        }
+        
+        for _ in benchmark.scaledIterations {
+            var params = Parameters()
+            _ = router.route(path: ["a"], parameters: &params)
+        }
+    }
+
+    Benchmark("Minimal Early Fail") { benchmark in
+        let router = TrieRouter.init(String.self)
+        for letter in ["aaaaaaaaaaaaaa"] {
+            router.register(letter, at: [
+                .constant(letter)
+            ])
+        }
+        
+        for _ in benchmark.scaledIterations {
+            var params = Parameters()
+            _ = router.route(path: ["baaaaaaaaaaaaa"], parameters: &params)
+        }
+    }
+}
