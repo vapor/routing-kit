a59d9433786d73e105a9b55563e3b91398b123a0
Paul Toffoloni <paultoffoloni@gmail.com>
2025-08-22 10:38:27 +0200

    Update benchmarks manifest

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "benchmarks",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v15)
     ],
     dependencies: [
         .package(path: "../"),
