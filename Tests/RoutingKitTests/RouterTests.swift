66298070e0f1b893fa9899b441e1ad32a4d85bec
Paul Toffoloni <69189821+ptoffy@users.noreply.github.com>
2025-11-25 10:39:14 +0100

    Allow empty paths when `.catchall`ing (#146)
    * Allow empty paths when `.catchall`ing
    
    * Add check
    
    * Add bug tracker to test
    
    * Update test name

--- Tests/RoutingKitTests/RouterTests.swift
+++ Tests/RoutingKitTests/RouterTests.swift
@@ -382,4 +382,14 @@ struct RouterTests {
         p = Parameters()
         #expect(r.route(path: ["y", "file.md"], parameters: &p) == 1)
     }
+
+    @Test("Catchall catches empty path", .bug("https://github.com/vapor/routing-kit/issues/147"))
+    func emptyCatchall() throws {
+        var b = TrieRouterBuilder<Int>()
+        b.register(1, at: [.catchall])
+        let r = b.build()
+
+        var p = Parameters()
+        #expect(r.route(path: [], parameters: &p) == 1)
+    }
 }
