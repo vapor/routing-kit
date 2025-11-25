c84d01f08538eec1426c003751352c3c1720c44c
Paul Toffoloni <69189821+ptoffy@users.noreply.github.com>
2025-11-25 12:17:37 +0100

    Update Sources/RoutingKit/TrieRouter/TrieRouterBuilder.swift
    Co-authored-by: Gwynne Raskind <gwynne@vapor.codes>

--- Sources/RoutingKit/TrieRouter/TrieRouterBuilder.swift
+++ Sources/RoutingKit/TrieRouter/TrieRouterBuilder.swift
@@ -119,9 +119,7 @@ public struct TrieRouterBuilder<Output: Sendable>: RouterBuilder {
             )
             return node.copyWith(wildcard: newWildcard)
         case .partialParameter(let template, let components, let parameters):
-            guard parameters.count == Set(parameters).count else {
-                preconditionFailure("Partial \":\(template)\" contains multiple same-named parameters")
-            }
+           precondition(parameters.count == Set(parameters).count, #"Partial ":\#(template)" contains multiple parameters with the same name"#)
             var partials = node.partials ?? []
             let child = partials.first(where: { $0.template == template })?.node ?? Node()
             let updatedChild = insertRoute(node: child, path: path.dropFirst(), output: output)
